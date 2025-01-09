3dd5dd878e34829b102c99f019a75410ad316ed4
Ali Mustafa <mohammadali7477@gmail.com>
2025-01-08 19:50:13 -0500

    Initial Commit

---
+++ bulletin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bulletin
+//
+//  Created by Ali Mustafa on 1/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bulletin/bulletinApp.swift
@@ -0,0 +1,17 @@
+//
+//  bulletinApp.swift
+//  bulletin
+//
+//  Created by Ali Mustafa on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct bulletinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
